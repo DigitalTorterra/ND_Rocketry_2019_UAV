34da6469c2aec4f266cdb8d8815449d49e5f682b
joegrip <30959775+joegrip@users.noreply.github.com>
2019-03-24 17:16:52 -0400

    Took out gps to meters

--- target_detection_3_18.py
+++ target_detection_3_18.py
@@ -207,30 +207,6 @@ class TargetDetection():
 			
 		return(features['Centroid'],svm_result)
 """	
-################################################################################################
-# function:    Get distance in meters
-# parameters:  Two global relative locations
-# returns:     Distance in meters
-################################################################################################
-def get_distance_meters(locationA, locationB):
-    # approximate radius of earth in km
-    R = 6373.0
-
-    lat1 = radians(locationA.lat)
-    lon1 = radians(locationA.lon)
-    lat2 = radians(locationB.lat)
-    lon2 = radians(locationB.lon)
-
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-    distance = (R * c) * 1000
-
-    print("Distance (meters):", distance)
-    return distance
 
 
 images = np.load('images.npy')
